05b4cd9ee7f71d8ba9167410c7bb5a44041f3f8e
Edward Evbert <edwardevbert@gmail.com>
2020-08-21 17:27:00 +0700

    Development

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
 
-mix.js('src/js/min.js', 'public/js')
-   .sass('src/sass/min.scss', 'public/css')
+mix.js('src/js/min.js', 'public/assets/js')
+   .sass('src/sass/min.scss', 'public/assets/css')
    .setPublicPath('public');
