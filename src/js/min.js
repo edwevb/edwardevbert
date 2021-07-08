9f069bd99de656d66c465b389575c08b9e82e426
edward evbert <edwardevbert@gmail.com>
2021-07-09 00:51:12 +0700

    update

--- src/js/min.js
+++ src/js/min.js
@@ -124,7 +124,6 @@ $.fn.materializeInputs = function(selectors) {
 		element.setAttribute('value', element.value);
 	}
 
-
 	this.find(selectors).each(function () {
 		setInputValueAttr(this);
 	});
@@ -134,25 +133,115 @@ $.fn.materializeInputs = function(selectors) {
 	});
 };
 
-$('form').on('submit', function(e){
+$('form').on('submit', async function(e){
 	e.preventDefault();
+	removeValidationElem();
 	const input = document.getElementsByClassName('form-control'),
+	url = 'https://edwardemailapi.herokuapp.com/api/contact',
 	data = {};
-	for (var i = 0; i < input.length; i++) {
+	for (let i = 0; i < input.length; i++) {
 		data[input[i].name] = input[i].value;
 	}
-	console.log(data);
+	await doAjax(data, url);
+});
+
+const doAjax = (data, url) => {
+	$.ajax({
+		type:'post',
+		url:url,
+		data:data,
+		dataType:'json',
+		beforeSend:() => {
+			requestPreLoader();
+		},
+		success: async (res) => {
+			await console.log(res.message)
+			await successAlert(res.message);
+			await requestPreLoaderRemove();
+		},
+		error: async (xhr) => {
+			console.log(xhr.responseText)
+			if (xhr.status == 422) {
+				await warningAlert(xhr);
+			}else{
+				await serverTimeOut();
+			}
+			await requestPreLoaderRemove();
+		}
+	});
+}
+
+const successAlert = (msg) =>{
 	Swal.fire({
 		icon: 'success',
 		iconColor:'#3F3D56',
-		title: 'Thank you for your feedback!',
-		html: `Your message has been send! <br> but to make sure, please also send manually to my email below. <br> <a href="mailto:edwardevbert@gmail.com" class="text-decoration-none">edwardevbert@gmail.com</a>`,
+		title: msg,
+		html: `Your message has been send! <br> to make sure, please also send manually to my email below. <br> <a href="mailto:edwardevbert@gmail.com" class="text-decoration-none">edwardevbert@gmail.com</a>`,
 		customClass:{
 			confirmButton: 'btn btn-dark px-5',
 		},
 		buttonsStyling:false
 	});
-});
+}
+
+const warningAlert = (res) => {
+	Swal.fire({
+		icon: 'error',
+		title: 'Oops.. something error!',
+		text: res.message,
+	}).then(()=>{
+		formValidationShow(res);
+	});
+	$('input[name=captcha]').val(null);
+}
+
+const serverTimeOut = () => {
+	Swal.fire({
+		icon: 'error',
+		title: 'Server lost connection!',
+		text: 'Please try again later',
+	});
+	$('input[name=captcha]').val(null);
+}
+
+const formValidationShow = (xhr) => {
+	const res = xhr.responseJSON;
+	let keys = "";
+	if ($.isEmptyObject(res) == false) {
+		$.each(res.errors, (key, value) => {
+			if(key.indexOf(".") != -1){
+				const arr = key.split(".");
+				keys = $(`#${arr[0]}`)
+			}else{
+				keys = $(`#${key}`)
+			}
+			keys.closest('.form-control')
+			.addClass('is-invalid');
+			keys.closest('.form-group')
+			.append(`<div class="invalid-feedback" style="position: absolute;">${value}</div>`)
+		});
+	}
+}
+
+const removeValidationElem = () => {
+	const form = $('#contact-form');
+	form.find('.invalid-feedback').remove();
+	form.find('.form-control').removeClass('is-invalid');
+}
+
+const requestPreLoader = async () =>{
+	await $('#btn-submit').prop('disabled', true);
+	await $('#btn-submit').css('cursor', 'not-allowed');
+	await $('#btn-submit').html(` <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+		Loading...`);
+	await grecaptcha.reset();
+}
+
+const requestPreLoaderRemove = async () =>{
+	await $('#btn-submit').html(`<i class="far fa-paper-plane"></i> Send`);
+	await grecaptcha.reset();
+}
+
 
 $('#myBlog').on('click', ()=>{
 	Swal.fire({
